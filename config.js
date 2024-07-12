aff33169766b4501da3eacd8a0cb925409f973b9
Sal Aiello <dested@gmail.com>
2024-07-11 17:53:18 -0700

    working test

--- config.js
+++ config.js
@@ -1,15 +1,25 @@
 module.exports = {
-  name: 'sde-test1',
+  name: 'sde-test2',
   entry: './src/index.ts',
   esbuildPlugins: [],
   port: 3000,
   esbuildExternals: [],
+  aws: {
+    region: 'us-west-2',
+    profile: 'quickgame',
+    cpu: 256,
+    concurrentExecutions: 3,
+    memory: 512,
+    healthCheckRoute: '/',
+    sslCertificateARN: 'arn:aws:acm:us-west-2:388098074003:certificate/fa3f6d37-1580-441b-a207-221a8d0579bf',
+    vpcId: 'vpc-06226d9bcb15e66d1',
+    accountId: '388098074003',
+    hostedZoneID: 'Z04016231YNZ02E3V4Y2F',
+    domainName: 'horse.quickga.me',
+    zoneName: 'quickga.me',
+  },
   envs: {
     prod: {
-      aws: {
-        region: 'us-west-2',
-        profile: 'quickgame',
-      },
       env: {
         NODE_ENV: 'development',
       },
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
+const appId = Math.random().toString(36).substring(7);
 async function main() {
   const app = express();
   app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello asdfasdf + ' + appId);
   });
   app.listen(3000, () => {
-    console.log('ServerAaasadaa ais da daon http://localhost:3000');
+    console.log('ServerAaasadaa ais da daon http://localhost:3000 + ' + appId);
   });
 }
 
